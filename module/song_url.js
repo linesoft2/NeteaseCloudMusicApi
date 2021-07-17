d964920799d4c7ef5f7cefcc23293042bf47f242
双霖 <l@linesoft.top>
2021-07-18 02:25:12 +0800

    增加跳转功能

--- module/song_url.js
+++ module/song_url.js
@@ -9,7 +9,7 @@ module.exports = (query, request) => {
     ids: '[' + query.id + ']',
     br: parseInt(query.br || 999000),
   }
-  return request(
+  const answer = request(
     'POST',
     `https://interface3.music.163.com/eapi/song/enhance/player/url`,
     data,
@@ -19,7 +19,12 @@ module.exports = (query, request) => {
       proxy: query.proxy,
       realIP: query.realIP,
       url: '/api/song/enhance/player/url',
-      redirect: query.redirect || false,
+//       redirect: query.redirect || false,
     },
   )
+  if (query.redirect){
+    return {jump:{address:answer.body.data[0].url}}
+  }else{
+    return answer
+  
 }
